39a62e8a7a2fc31417bd6d379a98b9f8f3d9a60d
Nate Mount <ngmount2021@gmail.com>
2022-05-28 23:00:07 -0400

    Removed App structure from Xengine main file

--- Xengine.c
+++ Xengine.c
@@ -2,18 +2,7 @@
 #include <string.h>
 #include <ncurses.h>
 #include <stdlib.h>
-
-#include "XengineConfig"
-
-typedef struct xengine_app {
-	int posX;
-	int posY;
-	int lenX;
-	int lenY;
-	char* (*getView)(void*);
-} * App;
-
-static App applications[MAX_APPLICATIONS + 1];
+#include "AppView.h"
 
 void xengine_init(){
 
@@ -24,35 +13,11 @@ void xengine_init(){
 
 }
 
-void xengine_renderWidget(App win){
-
-	int x;
-	int y;
-	char* view = win->getView(NULL);
-
-	for (y = 0; y < win->lenY; y++){
-
-		for (x = 0; x < win->lenX; x++){
-
-			mvprintw(win->posY + y, win->posX + x, "%c", view[x + (y * win->lenY) - 1]);
-
-		}
-
-	}
-
-}
-
 void xengine_renderView(){
 
-	int i;
-
-	for (i = 0; i < MAX_APPLICATIONS + 1; i++){
-		if (applications[i] != NULL){
-			xengine_renderWidget(applications[i]);
-		}
-	}
 
 }
+
 
 void xengine_viewManager(){
 
