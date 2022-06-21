9c1a4bc799d1a7fa73dee68e5e8692a94fdb8914
Nate Mount <ngmount2021@gmail.com>
2022-06-20 20:06:37 -0400

    Moved Xengine.c to src file

--- src/Xengine.c
+++ src/Xengine.c
@@ -2,18 +2,40 @@
 #include <string.h>
 #include <ncurses.h>
 #include <stdlib.h>
-#include "AppView.h"
+
+static WINDOW* CTRL_BAR;
+static int maxX, maxY;
+static int ch;
 
 void xengine_init(){
 
 	initscr();
 	raw();
 	noecho();
+	cbreak();
 	keypad(stdscr, TRUE);
+
+	getmaxyx(stdscr, maxY, maxX);
+
+	CTRL_BAR = newwin(2,maxX,maxY - 5, 0);
+	wborder(CTRL_BAR, 0, 0, '#', 0, 0, 0, 0, 0);
+	wrefresh(CTRL_BAR);
+	refresh();
+
+}
+
+void xengine_inputManager(){
+
+	while ((ch = getchar()) != '@'){
+
+		
+
+	}
 
 }
 
 void xengine_renderView(){
+
 
 
 }
@@ -30,13 +52,16 @@ void xengine_viewManager(){
 
 void xengine_terminate(){
 
+	wborder(CTRL_BAR,0,0,0,0,0,0,0,0);
+	wrefresh(CTRL_BAR);
+	delwin(CTRL_BAR);
 	endwin();
 
 }
 
 int main( int argc, char* argv[] ){
 
-	if ( strcmp( argv[argc - 1], HELP_FLAG ) == 0 ){
+	if ( strcmp( argv[argc - 1], "--help" ) == 0 ){
 		printf("Xengine Help\n");
 		exit(EXIT_SUCCESS);
 	}
